76c78d0af68959fad435944af5778aa8197bbea4
Suna <xkq3355@naver.com>
2024-07-28 19:03:16 +0900

    2024-07-28 리액트 필수강의 끝 심화과정 시작

--- class1/src/index.jsx
+++ class1/src/index.jsx
@@ -1,7 +1,12 @@
+import React from 'react';
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
+// JSX 사용하지 않는법
+//ReactDOM.createRoot(entryPoint).render(React.createElement(App));
+
+// JSX 사용법
 ReactDOM.createRoot(entryPoint).render(<App />);
